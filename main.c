d9989cc4991dfc1713f8217cf503fe8238dff6a8
GTMalachi <mwmuhic@yahoo.com>
2026-06-22 13:11:57 -0700

    Starting gradient calculations

--- main.c
+++ main.c
@@ -39,6 +39,62 @@ b32 mat_cross_entropy_add_grad(
     const matrix* p, const matrix* q, const matrix* grad
 );
 
+typedef enum {
+    MV_FLAG_NONE = 0,
+
+    MV_FLAG_REQUIRES_GRAD  = (1 << 0),
+    MV_FLAG_PARAMETER      = (1 << 1),
+    MV_FLAG_INPUT          = (1 << 2),
+    MV_FLAG_OUTPUT         = (1 << 3),
+    MV_FLAG_DESIRED_OUTPUT = (1 << 4),
+    MV_FLAG_COST           = (1 << 5),
+} model_var_flags;
+
+typedef enum {
+    MV_OP_NULL = 0,
+    MV_OP_CREATE,
+
+    _MV_OP_UNARY_START,
+
+    MV_OP_RELU,
+    MV_OP_SOFTMAX,
+
+    _MV_OP_BINARY_START,
+
+    MV_OP_ADD,
+    MV_OP_SUB,
+    MV_OP_MATMUL,
+    MV_OP_CROSS_ENTROPY,
+} model_var_op;
+
+#define MODEL_VAR_MAX_INPUTS 2
+#define MV_NUM_INPUTS(op) ((op) < _MV_OP_UNARY_START ? 0 : ((op) < _MV_OP_BINARY_START ? 1 : 2))
+
+typedef struct model_var {
+    u32 index;
+    u32 flags;
+
+    matrix* val;
+    matrix* grad;
+
+    // model_var_op op;
+    // struct model_var* inputs[MODEL_VAR_MAX_INPUTS];
+} model_var;
+
+typedef struct{
+    // pause
+} model_program;
+
+typedef struct {
+    u32 num_vars;
+
+    model_var* input;
+    model_var* output;
+    model_var* desired_output;
+    model_var* cost;
+
+} model_context;
+
 void draw_mnist_digit(f32* data);
 
 int main(void) {
